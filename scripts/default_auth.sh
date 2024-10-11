ca724897299fc364c1907a98ab321844fb88da06
Asten <0xasten@gmail.com>
2024-10-11 15:14:05 +0800

    add auth script

--- scripts/default_auth.sh
+++ scripts/default_auth.sh
@@ -2,23 +2,34 @@
 set -euo pipefail
 pushd $(dirname "$0")/..
 
-export RPC_URL="http://localhost:5050";
+: "${STARKNET_RPC_URL:?Environment variable STARKNET_RPC_URL must be set}"
 
-export WORLD_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.world.address')
+export WORLD_ADDRESS=$(cat ./manifests/release/deployment/manifest.json | jq -r '.world.address')
+export ACTIONS_ADDRESS=$(cat ./manifests/release/deployment/manifest.json | jq -r '.contracts[] | select(.tag == "Arena-actions" and .kind == "DojoContract").address')
+export FIGHT_STSTEM_ADDRESS=$(cat ./manifests/release/deployment/manifest.json | jq -r '.contracts[] | select(.tag == "Arena-fight_system" and .kind == "DojoContract").address')
 
-export ACTIONS_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "actions" ).address')
 
 echo "---------------------------------------------------------------------------"
-echo world : $WORLD_ADDRESS 
+echo world : $WORLD_ADDRESS
 echo " "
 echo actions : $ACTIONS_ADDRESS
+echo " "
+echo FIGHT_STSTEM : $FIGHT_STSTEM_ADDRESS
 echo "---------------------------------------------------------------------------"
 
-# enable system -> component authorizations
-COMPONENTS=("CharacterInfo" "Counter" "Arena" "ArenaCharacter" "ArenaRegistered" )
+# enable system -> models authorizations
+sozo auth grant --world $WORLD_ADDRESS --rpc-url $STARKNET_RPC_URL --wait writer \
+  model:Arena-ArenaCounter,$ACTIONS_ADDRESS \
+  model:Arena-Arena,$ACTIONS_ADDRESS \
+  model:Arena-ArenaCharacter,$ACTIONS_ADDRESS \
+  model:Arena-ArenaRegistered,$ACTIONS_ADDRESS \
+  model:Arena-CharacterInfo,$ACTIONS_ADDRESS
 
-for component in ${COMPONENTS[@]}; do
-    sozo auth writer $component $ACTIONS_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
-done
+sozo auth grant --world $WORLD_ADDRESS --rpc-url $STARKNET_RPC_URL --wait writer \
+  model:Arena-ArenaCounter,$FIGHT_STSTEM_ADDRESS \
+  model:Arena-Arena,$FIGHT_STSTEM_ADDRESS \
+  model:Arena-ArenaCharacter,$FIGHT_STSTEM_ADDRESS \
+  model:Arena-ArenaRegistered,$FIGHT_STSTEM_ADDRESS \
+  model:Arena-CharacterInfo,$FIGHT_STSTEM_ADDRESS
 
 echo "Default authorizations have been successfully set."
