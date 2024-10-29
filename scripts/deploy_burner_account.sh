2cccb09d9bf3a792880b80fa615896fe0e3e9fe8
Asten <0xasten@gmail.com>
2024-10-30 00:13:24 +0800

    update scripts

--- scripts/deploy_burner_account.sh
+++ scripts/deploy_burner_account.sh
@@ -12,3 +12,43 @@ starkli account deploy \
     --rpc $STARKNET_RPC_URL \
     --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7 \
     ./scripts/accounts/1
+
+starkli account deploy \
+    --rpc $STARKNET_RPC_URL \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7 \
+    ./scripts/accounts/2
+
+starkli account deploy \
+    --rpc $STARKNET_RPC_URL \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7 \
+    ./scripts/accounts/3
+
+starkli account deploy \
+    --rpc $STARKNET_RPC_URL \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7 \
+    ./scripts/accounts/4
+
+starkli account deploy \
+    --rpc $STARKNET_RPC_URL \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7 \
+    ./scripts/accounts/5
+
+starkli account deploy \
+    --rpc $STARKNET_RPC_URL \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7 \
+    ./scripts/accounts/6
+
+starkli account deploy \
+    --rpc $STARKNET_RPC_URL \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7 \
+    ./scripts/accounts/7
+
+starkli account deploy \
+    --rpc $STARKNET_RPC_URL \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7 \
+    ./scripts/accounts/8
+
+starkli account deploy \
+    --rpc $STARKNET_RPC_URL \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7 \
+    ./scripts/accounts/9
--- scripts/new_burner_account.sh
+++ scripts/new_burner_account.sh
@@ -8,5 +8,29 @@ echo "--------------------------------------------------------------------------
 echo RPC URL : $STARKNET_RPC_URL
 echo "---------------------------------------------------------------------------"
 
+starkli account oz init ./scripts/accounts/1 \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7
+
+starkli account oz init ./scripts/accounts/2 \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7
+
 starkli account oz init ./scripts/accounts/3 \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7
+
+starkli account oz init ./scripts/accounts/4 \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7
+
+starkli account oz init ./scripts/accounts/5 \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7
+
+starkli account oz init ./scripts/accounts/6 \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7
+
+starkli account oz init ./scripts/accounts/7 \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7
+
+starkli account oz init ./scripts/accounts/8 \
+    --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7
+
+starkli account oz init ./scripts/accounts/9 \
     --private-key 0x587692064670966047507699fbfbebb04e93531ca6d8a503519385fe0d2a3e7
